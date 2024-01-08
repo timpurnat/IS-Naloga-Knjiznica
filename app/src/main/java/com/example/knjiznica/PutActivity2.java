5235050843cc6ecc323afccb7bb7b04d4976303f
tajnsekaljaz <tajnsek32@email.com>
2024-01-08 16:55:41 +0100

    Android1 ui2

---
+++ app/src/main/java/com/example/knjiznica/PutActivity2.java
@@ -0,0 +1,14 @@
+package com.example.knjiznica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PutActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_put2);
+    }
+}
