7faa7536596a463ae39a3f916605b70dc70c576d
tajnsekaljaz <tajnsek32@email.com>
2024-01-16 22:05:28 +0100

    konec

--- web/Models/Kategorija.cs
+++
@@ -1,9 +0,0 @@
-namespace web.Models
-{
-    public class Kategorija
-    {
-        public int KategorijaID { get; set; }
-        public String? imeKategorije { get; set; }
-        public ICollection<Knjiga>? Knjige { get; set; }
-    }
-}
--- web/Models/Zvrst.cs
+++
@@ -1,9 +0,0 @@
-namespace web.Models
-{
-    public class Zvrst
-    {
-        public int ZvrstID { get; set; }
-        public String? ImeZvrsti { get; set; }
-        public ICollection<Knjiga>? Knjige { get; set; }
-    }
-}
